549d0267da8b95c07e41619d314acaa5bbd8da56
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2019-11-27 21:50:31 +1300

    Added update and render timing.
    The game can now update and render at a desired rate.

--- quickbuild.bat
+++ quickbuild.bat
@@ -1,1 +1,2 @@
 g++ -Iinclude\sdl2 src\robotman.cpp -Llib -lmingw32 -lsdl2main -lsdl2 -o robotman.exe
+PAUSE
--- src/robotman.cpp
+++ src/robotman.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-#include "SDL.h"
+#include <SDL.h>
+#include <windows.h>
 
-const int w_width = 600, w_height = 600;
+const int w_width = 600, w_height = 600, desired_fps = 60;
 SDL_Window * window;
 SDL_Renderer * rr;
+SDL_Event event;
+
+bool closed = false;
+
+void update() {
+	if(SDL_PollEvent(&event)) {
+		if(event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE) {
+			closed = true;
+		}
+	}
+}
+
+void render() {
+	
+}
 
 int main(int argc, char** argv) {
 	if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
@@ -12,13 +28,36 @@ int main(int argc, char** argv) {
 	window = SDL_CreateWindow("Robot Man", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w_width, w_height, 0);
 	rr = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	
-	bool closed = false;
-	SDL_Event event;
+	DWORD now = GetTickCount(), then = now, timer = now;
+	double millisPerUpdate = 1000.0 / desired_fps;
+	double updatesQueued = 0.0;
+	bool canRender = false;
+	
+	int updates = 0, frames = 0;
+	
 	while(!closed) {
-		if(SDL_PollEvent(&event)) {
-			if(event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE) {
-				closed = true;
-			}
+		now = GetTickCount();
+		updatesQueued += (now - then) / millisPerUpdate;
+		then = now;
+		
+		while(updatesQueued > 0) {
+			update();
+			updatesQueued--;
+			updates++;
+			canRender = true;
+		}
+		
+		if(canRender) {
+			render();
+			frames++;
+			canRender = false;
+		}
+		
+		if(now - timer >= 5000.0) {
+			timer = now;
+			std::cout << "Updates: " << updates / 5 << ", Frames: " << frames / 5 << std::endl;
+			updates = 0;
+			frames = 0;
 		}
 	}
 	SDL_Quit();
